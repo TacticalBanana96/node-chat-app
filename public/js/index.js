cb7e7dfcfa5c1a30e06c1bdf2b8d11bd5feacd62
Daria Gonsalves <daria.gonsalves@hyuna.bb>
2018-09-07 12:00:04 -0400

    Add newMessage and createmessage events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+const socket = io();
+
+socket.on('connect', function () { //AVOID USING ES6 ARROW FUNCTIONS IN CLIENT SIDE JS
+  console.log('Connected to server');
+
+  //   socket.emit('createEmail', {
+  //     to: 'jen@example.com',
+  //     text: 'Hey this is Daria'
+  //   });
+  // });
+
+
+  // socket.on('newEmail', function (email) {
+  //   console.log('New email', email);
+  // });
+  socket.emit('createMessage', {
+    from: 'Somebody',
+    text: 'psk;skddk;'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+socket.on('newMessage', function (message) {
+  console.log('Message', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,14 +1,45 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
-
+const server = http.createServer(app); //createServer is also called by express.listen
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+io.on('connection', (socket) =>{ //registers an event listener
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'example',
+    text: 'sup bro',
+    createdAt: 12456789
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+  // socket.emit('newEmail', {
+  //   from: 'mike@example.com',
+  //   text: 'Hey. sup bruh',
+  //   createAt: 123
+  // });
+  //
+  //
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('createEmail', newEmail);
+  // });
+
+  socket.on('disconnect', () => {
+    console.log('Client Disconnected');
+  });
+});
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 // console.log(__dirname + '/../public');
