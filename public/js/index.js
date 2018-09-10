1341a534241ff06b4674f7c61df9e9f8e2bf8391
Daria Gonsalves <daria.gonsalves@hyuna.bb>
2018-09-10 08:55:07 -0400

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,21 +3,6 @@ const socket = io();
 socket.on('connect', function () { //AVOID USING ES6 ARROW FUNCTIONS IN CLIENT SIDE JS
   console.log('Connected to server');
 });
-  //   socket.emit('createEmail', {
-  //     to: 'jen@example.com',
-  //     text: 'Hey this is Daria'
-  //   });
-  // });
-
-
-  // socket.on('newEmail', function (email) {
-  //   console.log('New email', email);
-  // });
-  // socket.emit('createMessage', {
-  //   from: 'Somebody',
-  //   text: 'psk;skfsdfddk;'
-  // });
-
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
@@ -25,3 +10,17 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
   console.log('Message', message);
 });
+//   socket.emit('createEmail', {
+//     to: 'jen@example.com',
+//     text: 'Hey this is Daria'
+//   });
+// });
+
+
+// socket.on('newEmail', function (email) {
+//   console.log('New email', email);
+// });
+// socket.emit('createMessage', {
+//   from: 'Somebody',
+//   text: 'psk;skddk;'
+// });
--- server/server.js
+++ server/server.js
@@ -20,20 +20,32 @@ io.on('connection', (socket) =>{ //registers an event listener
   //   text: 'sup bro',
   //   createdAt: 12456789
   // });
+  socket.emit('newMessage', { //io.emit emits to all users
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
+      createdAt: new Date().getTime()
+  });
+
 
   socket.on('createMessage', (message) => {
-    //console.log('createMessage', message);
-    // io.emit('newMessage', { //io.emit emits to all users
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
-
-    socket.broadcast.emit('newMessage', {
-        from: message.from,
-        text: message.text,
-        createdAt: new Date().getTime()
+    console.log('createMessage', message);
+    io.emit('newMessage', { //io.emit emits to all users
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    // });
   });
   // socket.emit('newEmail', {
   //   from: 'mike@example.com',
